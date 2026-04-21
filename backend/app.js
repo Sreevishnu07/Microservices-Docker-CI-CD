782a09f9e75fbc058cefa5905a2378eeefe540b7
Sreevishnu07 <152363871+Sreevishnu07@users.noreply.github.com>
2026-04-21 17:51:38 +0530

    practices app.js

--- backend/app.js
+++ backend/app.js
@@ -1,43 +1,35 @@
-const express = require("express");
-const mysql = require("mysql2");
-
-const app = express();
-const PORT = 5000;
-
+const express=require("express");
+const mysql=require("mysql2");
+const app=express();
+const PORT=5000;
 let db;
-
-function connectWithRetry() {
-  console.log("Trying to connect to DB...");
-
-  db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+function connectionWithRetry(){
+  console.log("Trying to connect to db");
+  db=mysql.createConnection({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
   });
-
-  db.connect((err) => {
-    if (err) {
-      console.log("DB not ready, retrying in 3 seconds...");
-      setTimeout(connectWithRetry, 3000);
-    } else {
-      console.log("Connected to MySQL!");
+  db.connect((err)=>{
+    if(err){
+      console.log("DB not ready,try again in 3sec");
+      setTimeout(connectionWithRetry,3000);
+    }else{
+      console.log("Connected to MYSQL");
     }
   });
 }
-
-connectWithRetry();
-
-app.get("/api/data", (req, res) => {
-  db.query("SELECT 'Hello from MySQL' AS message", (err, results) => {
-    if (err) {
-      res.status(500).send("DB Error");
+connectionWithRetry();
+app.get("/api/data",(req,res)=>{
+  db.query("SELECT 'Hello from mysql' AS message",(err,results)=>{
+    if(err){
+      res.status(500).send("DB error");
       return;
     }
     res.json(results);
   });
 });
-
-app.listen(PORT, () => {
-  console.log(`Backend running on port ${PORT}`);
+app.listen(PORT,()=>{
+  console.log(`Backend running on ${PORT}`);
 });
